a5a03c6a3c3db1f9b3211e1e6c9f1b8d81116b00
Chedlywerda98 <chedly.werda@esprit.tn>
2023-05-11 18:23:22 +0100

    second commit

--- app.js
+++ app.js
@@ -6,25 +6,43 @@ const passport			= require('passport');
 const localStrategy		= require('passport-local').Strategy;
 const bcrypt			= require('bcrypt');
 const app				= express();
+const router            = express.Router();
 
-mongoose.connect("mongodb+srv://chedlywerda:oIDqDC7FfNABYLlV@cluster0.fjsrksp.mongodb.net/ChatBot",
- { useNewUrlParser: true, useUnifiedTopology: true })
-   .catch((error) => {
-    console.log("Error connecting to MongoDB Atlas: " + error);
-  });
+
+
+
+
+mongoose.connect("mongodb+srv://chedlywerda:oIDqDC7FfNABYLlV@cluster0.fjsrksp.mongodb.net/ChatBot", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+}).catch((error) => {
+	console.log("Error connecting to MongoDB Atlas: " + error);
+});
 
 const db = mongoose.connection;
 
 db.once("open", () => {
-  console.log("Connected to MongoDB database 'test'");
+	console.log("Connected to MongoDB database 'test'");
 });
 
 db.on("error", (error) => {
-  console.log("MongoDB connection error: " + error);
+	console.log("MongoDB connection error: " + error);
 });
 
 
-const users = mongoose.model('Users',{
+const conversationSchema = new mongoose.Schema({
+  user_input: [{ type: String, required: true }],
+  bot_response: { type: String, required: true }
+}, { collection: 'Convs' });
+
+const Conversation = mongoose.model('Conversation', conversationSchema);
+
+module.exports = Conversation;
+
+
+
+
+const users = mongoose.model('Users', {
 	username: {
 		type: String,
 		required: true
@@ -35,10 +53,15 @@ const users = mongoose.model('Users',{
 	}
 });
 
+  
 
 
 // Middleware
-app.engine('hbs', hbs({ extname: '.hbs' }));
+app.engine('hbs', hbs({ extname: '.hbs', 
+		runtimeOptions: {
+			allowProtoPropertiesByDefault: true,
+		}
+	}));
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 app.use(session({
@@ -59,40 +82,36 @@ passport.serializeUser(function (users, done) {
 
 passport.deserializeUser(function (id, done) {
 	users.findById(id)
-	  .then(function(users) {
-		done(null, users);
-	  })
-	  .catch(function(err) {
-		done(err);
-	  });
-  });
-  passport.use(new localStrategy(async function (username, password, done) {
+		.then(function (users) {
+			done(null, users);
+		})
+		.catch(function (err) {
+			done(err);
+		});
+});
+passport.use(new localStrategy(async function (username, password, done) {
 	try {
-	  const user = await users.findOne({ username: username });
-	  
-	  if (!user) { 
-		return done(null, false, { message: 'Incorrect username or password.' });
-	  }
-  
-	  const passwordMatch = await bcrypt.compare(password, user.password);
-	  
-	  if (!passwordMatch) { 
-		return done(null, false, { message: 'Incorrect username or password.' });
-	  }
-  
-	  return done(null, user);
+		const user = await users.findOne({ username: username });
+
+		if (!user) {
+			return done(null, false, { message: 'Incorrect username or password.' });
+		}
+
+		const passwordMatch = await bcrypt.compare(password, user.password);
+
+		if (!passwordMatch) {
+			return done(null, false, { message: 'Incorrect username or password.' });
+		}
+
+		return done(null, user);
 	} catch (err) {
-	  return done(err);
+		return done(err);
 	}
-  }));
-  
-
-
+}));
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) return next();
 	res.redirect('/login');
-	
 }
 
 function isLoggedOut(req, res, next) {
@@ -100,14 +119,38 @@ function isLoggedOut(req, res, next) {
 	res.redirect('/');
 }
 
+
 // ROUTES
 app.get('/', isLoggedIn, (req, res) => {
-	res.render("index", { title: "home" });
-});
+	res.render("index", { title: "Conversations" });
+  });
 
-app.get('/about',isLoggedIn, (req, res) => {
-	res.render("index", { title: "About" });
-});
+
+  app.get('/conversations', isLoggedIn, function(req, res) {
+	Conversation.find().then(function(conversations) {
+	  res.render('conversations', { conversations: conversations });
+	}).catch(function(error) {
+	  console.log(error);
+	  res.status(500).send('Internal Server Error');
+	});
+  });
+  
+
+
+// Delete conversation
+router.delete('/conversations/:id', async (req, res) => {
+	try {
+	  const deletedConv = await Conversation.findByIdAndDelete(req.params.id);
+	  if (!deletedConv) {
+		return res.status(404).send();
+	  }
+	  res.send(deletedConv);
+	} catch (error) {
+	  res.status(500).send(error);
+	}
+  });
+
+
 
 app.get('/login', isLoggedOut, (req, res) => {
 	const response = {
@@ -138,3 +181,5 @@ app.get('/logout', function(req, res){
 app.listen(3000, () => {
 	console.log("Listening on port 3000");
 });
+
+
